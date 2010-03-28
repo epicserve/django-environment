620c1a8e116e82bc0f299b215b501e600ee58b74
Brent O'Connor <epicserve@gmail.com>
2010-03-27 21:28:52 -0500

    Made it so it creates your django environment automatically when you setup a virtualenv

--- django_env/utils/django_settings.py
+++ django_env/utils/django_settings.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
-import os
+#!/usr/bin/env python
+
+import sys, os
+
 project_name = os.path.basename(os.getenv("VIRTUAL_ENV"))
 
-try:
-	from django.conf import settings
-except ImportError:
-	try:
-		exec "from %s.config import settings" % project_name
-	except ImportError:
-		exec "from %s import settings" % project_name
+# try:
+# 	exec "from %s.config import settings" % project_name
+# except ImportError:
+# 	from django.conf import settings
 
 def get_var(var_name, default=''):
 	val = getattr(settings, var_name, default)
@@ -18,8 +17,9 @@ def get_path(var_name):
 	val = get_var(var_name, [])
 	val = ":".join(val)
 	print val
-	
+
 if __name__ == '__main__':
-	import sys
+	
+	exec "from %s.config import local_settings as settings" % project_name
 	
 	locals()[sys.argv[1]](sys.argv[2])
